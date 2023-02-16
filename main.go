b9f7e46fb6a005d545dcbb8335875ee053083b0f
Soumya Sourav Guha <codecallisto@gamil.com>
2023-02-16 10:39:06 +0530

    Main.go coded till user's inputs

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var conferenceName string = "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
+	// go through Data tyoes in go.<<-uint etc..-->>
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("we have total of %v Tickets and %v Are still remaining\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
+	var firstName string
+	var userTickets uint
+	var lastName string
+	var midName string
+	var email string
+
+	//ask user for their name
+	fmt.Println("Enter your first name")
+	fmt.Scan(&firstName)
+	fmt.Println("enter your midname")
+	fmt.Scan(&midName)
+	fmt.Println("enter your last name")
+	fmt.Scan(&lastName)
+	fmt.Println("enter your email")
+	fmt.Scan(&email)
+	//use of pointer.Memory address//Special varialbe//println(variable) to print memory address
+	fmt.Println("Enter no of tickets")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+
+	fmt.Printf(" %v booked %v tickets", firstName, userTickets)
+	fmt.Println("	you will get a confirmation at this email address")
+	fmt.Println("Remaining tickets left	", remainingTickets)
+}
