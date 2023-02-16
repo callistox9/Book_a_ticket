22424a3974d5be147fd9d537a5df342b1b3dfae7
Soumya Sourav Guha <codecallisto@gamil.com>
2023-02-16 14:27:36 +0530

    infinite loop

--- main.go
+++ main.go
@@ -11,36 +11,41 @@ func main() {
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("we have total of %v Tickets and %v Are still remaining\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
-
-	var firstName string
-	var userTickets uint
-	var lastName string
-	var midName string
-	var email string
-
-	//ask user for their name
-	fmt.Println("Enter your first name")
-	fmt.Scan(&firstName)
-	fmt.Println("enter your midname")
-	fmt.Scan(&midName)
-	fmt.Println("enter your last name")
-	fmt.Scan(&lastName)
-	fmt.Println("enter your email")
-	fmt.Scan(&email)
-	//use of pointer.Memory address//Special varialbe//println(variable) to print memory address
-	fmt.Println("Enter no of tickets")
-	fmt.Scan(&userTickets)
-
-	remainingTickets = remainingTickets - userTickets
-	//Arrays and slices
-
-	//var bookings [50]string
 	var bookings []string // syntax var bookings =[]string{}; or bookings:=[]string{};
-	bookings = append(bookings, firstName+" "+lastName)
+	// Infinite loop
+	for {
+		var firstName string
+		var userTickets uint
+		var lastName string
+		var midName string
+		var email string
 
-	fmt.Printf(" %v booked %v tickets", firstName, userTickets)
-	fmt.Println("	you will get a confirmation at this email address")
-	fmt.Printf("These are all your bookings %v\n", bookings)
+		//ask user for their name
+		fmt.Println("Enter your first name")
+		fmt.Scan(&firstName)
+		fmt.Println("enter your midname")
+		fmt.Scan(&midName)
+		fmt.Println("enter your last name")
+		fmt.Scan(&lastName)
+		fmt.Println("enter your email")
+		fmt.Scan(&email)
+		//use of pointer.Memory address//Special varialbe//println(variable) to print memory address
+		fmt.Println("Enter no of tickets")
+		fmt.Scan(&userTickets)
 
-	fmt.Println("Remaining tickets left	", remainingTickets)
+		remainingTickets = remainingTickets - userTickets
+		//Arrays and slices
+
+		//var bookings [50]string
+
+		bookings = append(bookings, firstName+" "+lastName)
+
+		fmt.Printf("\n %v booked %v tickets", firstName, userTickets)
+		fmt.Println("	you will get a confirmation at this email address")
+		fmt.Printf("These are all your bookings %v\n", bookings)
+
+		fmt.Println("Remaining tickets left	", remainingTickets)
+
+	}
+
 }
